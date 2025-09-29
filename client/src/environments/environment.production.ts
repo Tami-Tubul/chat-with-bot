88616c652639903b2083b2e6c576c0e2740308c0
Tami Tubul <tamit0430@gmail.com>
2025-09-29 10:19:39 +0300

    set railway api

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://chat-with-bot-server-production.up.railway.app'
-  apiUrl: 'https://chat-with-bot-server.onrender.com'
+  apiUrl: 'https://chat-with-bot-server-production.up.railway.app'
+  // apiUrl: 'https://chat-with-bot-server.onrender.com'
 };

0a1285d1977c10aeacd5f3a54776f16d71eac290
Tami Tubul <tamit0430@gmail.com>
2026-04-22 17:01:50 +0300

    switch deployment from vercel to render

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://chat-with-bot-server-production.up.railway.app'
-  apiUrl: 'https://chat-with-bot-server.vercel.app'
+  // apiUrl: 'https://chat-with-bot-server.vercel.app'
+  apiUrl: 'https://chat-with-bot-server.onrender.com'
 };
